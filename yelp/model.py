2f3699be761a47a955b079fcda492590ae8fcf7b
Nukezhanov Yernar (uib55591) <yernar.nukezhanov@conti-engineering.com>
2020-06-13 22:37:22 +0200

    Used monkeyLearn API to rate a review

--- yelp/model.py
+++ yelp/model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pymongo import MongoClient
 from functions import db_connect, get_collection, rate_review
+from monkeylearn import MonkeyLearn
 
 # establing connection
 client = db_connect()
@@ -24,3 +25,9 @@ for index, row in df_reviews.iterrows():
 
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
 	print(df_reviews['text'])
+
+ml = MonkeyLearn('685d6ec80be3bea9502c9103a3c6eb5096cb866a')
+data = ["I am updating my original review (below) as their management replied trying to play innocent. What they failed to mention is that we called to make a..."]
+model_id = 'cl_pi3C7JiL'
+result = ml.classifiers.classify(model_id, data)
+print(result.body)
