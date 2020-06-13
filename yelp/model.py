f5de3d288a2b39ec807b4325cf73b8c7b474ac47
Nukezhanov Yernar (uib55591) <yernar.nukezhanov@conti-engineering.com>
2020-06-13 16:53:30 +0200

    Retrieve statistics from MongoDB

--- yelp/model.py
+++ yelp/model.py
@@ -1,4 +1,23 @@
-import pymongo
-import json
+from pymongo import MongoClient
 
-client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0-89mew.mongodb.net/Cluster0?retryWrites=true&w=majority")
+# establing connection
+try:
+    client = MongoClient("mongodb+srv://admin:admin@cluster0-89mew.mongodb.net/TestDatabase?retryWrites=true&w=majority")
+    print("Connected successfully!!!")
+except:
+    print("Could not connect to MongoDB")
+
+db = client.get_database()
+assert db.name == 'TestDatabase'
+
+try:
+	collection = db['Testdata']
+	print("Connected to the collection")
+except:
+	print("Could not connect")
+
+# print collection statistics
+print(db.command("collstats", "events"))
+
+# print database statistics
+print(db.command("dbstats"))
