83a05f12981a7a4ebb45d9b8492a977df09fa18b
khalilovs <server.khalilov@carsync.de>
2020-06-13 16:00:01 +0200

    upd: database;

--- yelp/mongodb.py
+++ yelp/mongodb.py
@@ -1,4 +1,28 @@
-import pymongo
+from pymongo import MongoClient
 
-client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0-89mew.mongodb.net/Cluster0?retryWrites=true&w=majority")
-db = client.test
+# establing connection
+try:
+    client = MongoClient("mongodb+srv://admin:admin@cluster0-89mew.mongodb.net/TestDatabase?retryWrites=true&w=majority")
+    print("Connected successfully!!!")
+except:
+    print("Could not connect to MongoDB")
+
+db = client.get_database()
+assert db.name == 'TestDatabase'
+
+collection = db.create_collection('TestCreatedNow')
+
+document1 = {
+        "name":"John",
+        "age":24,
+        "location":"New York"
+        }
+#second document
+document2 = {
+        "name":"Sam",
+        "age":21,
+        "location":"Chicago"
+        }
+
+collection.insert_one(document1)
+collection.insert_one(document2)
