964044fac273090502110db2c995f889191db05e
john-jerome <31763328+john-jerome@users.noreply.github.com>
2020-06-13 15:45:37 +0200

    added moch api call

--- yelp/__main__.py
+++ yelp/__main__.py
@@ -1,1 +1,12 @@
-# executable file of the project: python -m myproject
+import requests
+
+url = "https://api.yelp.com/v3/events"
+
+payload = {}
+headers = {
+  'Authorization': 'Bearer EST24WiV5UEu-BGZFd7vKtHGcAUa7-rn4Pl0N_a9SVBqvOpYmlezH44rtYxtZQ7oDl6KtA2uNuBqlfha_WJKRsbwElJI_-iWZIXDo01KERC1rJ_nksJecLYch6TjXnYx'
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
